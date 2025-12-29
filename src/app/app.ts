d24a4958ae128f815a97f36b471e528588d6221e
Janani-thavaraja <janani@fieldex.com>
2025-12-29 19:56:20 +0530

    updates of text and projects

--- src/app/app.ts
+++ src/app/app.ts
@@ -108,14 +108,16 @@ export class App implements OnInit {
   phoneNumber = signal("+94754019752");
   linkedinUrl = signal("https://www.linkedin.com/in/janani-thavarajah-52987a1b2");
   githubUrl = signal("https://github.com/Jaanulittleluv");
-  cvUrl = signal("https://drive.google.com/file/d/1L1OYkD8VjowaDCyGqJvumsRi0jtMPdVF/view?usp=sharing");
-  prj1URL = signal("https://github.com/Jaanulittleluv/BookRates");
-  prj2URL = signal("https://github.com/Jaanulittleluv/BookApp");
-  prj3URL = signal("https://github.com/Jaanulittleluv/Cypress");
-  prj4URL = signal("https://github.com/Jaanulittleluv/Selenium");
-  prj5URL = signal("https://github.com/Jaanulittleluv/SpringBoot");
-  prj6URL = signal("https://github.com/Jaanulittleluv/sql-employee-data-management");
-  prj7URL = signal("https://github.com/Jaanulittleluv/Bear-Jump");
+  cvUrl = signal("https://drive.google.com/file/d/1nH4anfVt5A9vgJylFLxOC80KOxxyV0-G/view?usp=sharing");
+  prj1URL = signal("https://github.com/Jaanulittleluv/HearCall");
+  prj2URL = signal("https://github.com/Jaanulittleluv/FormApp-NodeJS-");
+  prj3URL = signal("https://github.com/Jaanulittleluv/BookRates");
+  prj4URL = signal("https://github.com/Jaanulittleluv/BookApp");
+  prj5URL = signal("https://github.com/Jaanulittleluv/Cypress");
+  prj6URL = signal("https://github.com/Jaanulittleluv/Selenium");
+  prj7URL = signal("https://github.com/Jaanulittleluv/SpringBoot");
+  prj8URL = signal("https://github.com/Jaanulittleluv/sql-employee-data-management");
+  prj9URL = signal("https://github.com/Jaanulittleluv/Bear-Jump");
 
   async copyPhoneNumber() {
     try {
@@ -212,18 +214,20 @@ export class App implements OnInit {
   imagepath = signal("https://www.svgrepo.com/show/353396/angular-icon.svg");
   imagepath1 = signal("https://www.svgrepo.com/show/452092/react.svg");
   imagepath2 = signal("https://www.svgrepo.com/show/452091/python.svg");
-  imagepath3 = signal("https://www.svgrepo.com/show/354321/selenium.svg");
-  imagepath4 = signal("https://www.svgrepo.com/show/374270/cypress.svg");
+  imagepath3 = signal("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_DarBcrJVsDdTMLc7-eMRqG28YYDDeCvdkw&s");
+  imagepath4 = signal("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU-L9SzX9DNWbVPRs2aO6DcgGi2-TOHXNwIA&s");
   imagepath5 = signal("https://www.svgrepo.com/show/354380/spring-icon.svg");
   imagepath6 = signal("https://uxwing.com/wp-content/themes/uxwing/download/file-and-folder-type/file-json-color-green-icon.png");
   imagepath7 = signal("https://www.svgrepo.com/show/303293/bootstrap-4-logo.svg");
   imagepath8 = signal("https://www.svgrepo.com/show/354202/postman-icon.svg");
+  imagepath18 = signal("https://www.svgrepo.com/show/452075/node-js.svg");
   imagepath9 = signal("https://www.svgrepo.com/show/303388/java-4-logo.svg");
   imagepath10 = signal("https://www.svgrepo.com/show/374144/typescript.svg");
   imagepath11 = signal("https://www.svgrepo.com/show/452228/html-5.svg");
   imagepath12 = signal("https://www.svgrepo.com/show/452185/css-3.svg");
   imagepath13 = signal("https://staging.svgrepo.com/show/353935/jira.svg");
   imagepath14 = signal("https://staging.svgrepo.com/show/353935/jira.svg");
+  imagepath17 = signal("https://staging.svgrepo.com/show/331488/mongodb.svg");
   imagepath15 = signal("https://www.svgrepo.com/show/303251/mysql-logo.svg");
   imagepath16 = signal("https://www.svgrepo.com/show/303210/figma-1-logo.svg");
 }
